cb598d51db3533fd0b24b6c342601e0892f48844
Bradley Bossard <bradleybossard@gmail.com>
2022-01-04 15:41:28 -0800

    add projection step

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 curl 'https://www2.census.gov/geo/tiger/GENZ2014/shp/cb_2014_06_tract_500k.zip' -o cb_2014_06_tract_500k.zip
 unzip -o cb_2014_06_tract_500k.zip
+
 npm install -g shapefile
 shp2json cb_2014_06_tract_500k.shp -o ca.json
 
+npm install -g d3-geo-projection
+geoproject 'd3.geoConicEqualArea().parallels([34, 40.5]).rotate([120, 0]).fitSize([960, 960], d)' < ca.json > ca-albers.json
