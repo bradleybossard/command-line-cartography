de0724e505b07581e9a390b6019edf735008db1e
Bradley Bossard <bradleybossard@gmail.com>
2022-01-04 15:37:51 -0800

    add command to create geojson

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
 curl 'https://www2.census.gov/geo/tiger/GENZ2014/shp/cb_2014_06_tract_500k.zip' -o cb_2014_06_tract_500k.zip
 unzip -o cb_2014_06_tract_500k.zip
+npm install -g shapefile
+shp2json cb_2014_06_tract_500k.shp -o ca.json
+
