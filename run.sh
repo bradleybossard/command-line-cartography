977ee963f9fcc448c515dc1cebd70ab4c9ea4731
Bradley Bossard <bradleybossard@gmail.com>
2022-01-04 15:42:30 -0800

    add comments to each step

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
+# get data
 curl 'https://www2.census.gov/geo/tiger/GENZ2014/shp/cb_2014_06_tract_500k.zip' -o cb_2014_06_tract_500k.zip
 unzip -o cb_2014_06_tract_500k.zip
 
+# convert shapefile to geojson
 npm install -g shapefile
 shp2json cb_2014_06_tract_500k.shp -o ca.json
 
+# reproject geojson
 npm install -g d3-geo-projection
 geoproject 'd3.geoConicEqualArea().parallels([34, 40.5]).rotate([120, 0]).fitSize([960, 960], d)' < ca.json > ca-albers.json
